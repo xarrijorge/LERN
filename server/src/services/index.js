d8bad4e3115be5fd81ed0037a851321b8f08c1a4
Xarri Jorge <kodinglife@gmail.com>
2020-02-16 23:07:13 +0000

    Created Messages & reports services

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,5 +1,9 @@
 const users = require('./users/users.service.js');
+const messages = require('./messages/messages.service.js');
+const reports = require('./reports/reports.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
+  app.configure(messages);
+  app.configure(reports);
 };
