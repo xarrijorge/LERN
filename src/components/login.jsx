b5e24c0b2791a6b6a41948dc0e9f2c063457031b
Xarri Jorge <kodinglife@gmail.com>
2020-03-09 05:12:58 +0000

    updated login form

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
+import { Form, Input, Button, Checkbox, Typography } from 'antd'
+
+const { Title } = Typography
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 }
+}
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 }
+}
 
 export default props => {
+  const onFinish = values => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo)
+  }
   return (
-    <div>
-      <input type="text" />
+    <div className="loginForm">
+      <Form
+        className="form"
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        size="large"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <div className="loginTitle">
+        <Title>Liberia Peacebuilding Office</Title>
+      </div>
     </div>
   )
 }
