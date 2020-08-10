3f45e4dd5dc098a2f97ab848c9dc23dcbee7678a
Xarri Jorge <kodinglife@gmail.com>
2020-08-10 19:41:24 +0000

    Replaced ?? with ||

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const app = http.createServer((req, res) => {
   res.end('Hello World from LERN')
 })
 
-const PORT = process.env.PORT ?? 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => `Server running on port ${PORT}`)
