a5fa640587ce2b1263b1965939b014900dc7e526
Xarri Jorge <kodinglife@gmail.com>
2020-08-10 19:31:52 +0000

    Added new express stat process to index.js

--- index.js
+++ index.js
@@ -7,6 +7,5 @@ const app = http.createServer((req, res) => {
 })
 
 const PORT = process.env.PORT ?? 3001
-const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => `Server running on port ${PORT}`)
